200630879c532b507caf74e8b45956495095c8dc
qiuzl <819685619@qq.com>
2024-05-25 14:56:29 +0800

    pass stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -55,12 +55,9 @@ int main(int argc, char **argv) {
   
   char buffer[1024] = {0};
   while(true){
-    read(client_fd, buffer, 15);
+    read(client_fd, buffer, 1024);
     if (memcmp(buffer, "*1\r\n$4\r\nping\r\n", 15) == 0) {
         send(client_fd, "+PONG\r\n", 7, 0);
-    }else
-    {
-      break;
     }
   }
 
